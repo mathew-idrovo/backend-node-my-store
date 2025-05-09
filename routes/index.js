7033d9ef825733a681ee3a83fe008a8b0d176991
Mathew <mateoicecua2000@gmail.com>
2025-05-09 16:30:29 -0500

    feat(customer): add customer model, route, schema, and service 👥🔌

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const productRouter = require('./products.router');
+const userRouter = require('./users.router');
+const customerRouter = require('./customers.router');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/products', productRouter);
+  router.use('/users', userRouter);
+  router.use('/customers', customerRouter);
 }
 
 module.exports = routerApi;
---
+++ routes/users.router.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const UserService = require('../services/user.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const {
+  createUserSchema,
+  updateUserSchema,
+  getUserSchema,
+} = require('../schemas /user.schema');
+
+const router = express.Router();
+const service = new UserService();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await service.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+router.post(
+  '/',
+  validatorHandler(createUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newUser = await service.create(body);
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+module.exports = router;
