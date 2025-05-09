a1bc7c85f3fb2d3b527ce4138273fb9a3acbdaf6
Mathew <mateoicecua2000@gmail.com>
2025-05-09 16:14:39 -0500

    chore(sequelize): setup ORM and migrations 🛠️🗄️

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,16 +1,35 @@
+// const { Sequelize } = require('sequelize');
+
+// const { config } = require('./../config/config');
+// const setupModels = require('../db/models');
+
+// const USER = encodeURIComponent(config.dbUser);
+// const PASSWORD = encodeURIComponent(config.dbPassword);
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+// const sequelize = new Sequelize(URI, {
+//   dialect: 'postgres',
+//   logging: false,
+// });
+// setupModels(sequelize);
+// console.log('[SEQUELIZE] Connection to DB established', USER, URI);
+
+// module.exports = sequelize;
+
+require('dotenv').config(); // ¡primera línea!
+
 const { Sequelize } = require('sequelize');
+const setupModels = require('../db/models');
 
-const { config } = require('./../config/config');
-const setupModels = require('../db/model');
+const USER = encodeURIComponent(process.env.DB_USER);
+const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+const HOST = process.env.DB_HOST;
+const PORT = process.env.DB_PORT;
+const DB = process.env.DB_NAME;
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DB}`;
 
-const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
-  logging: false,
-});
+const sequelize = new Sequelize(URI, { dialect: 'postgres', logging: false });
 setupModels(sequelize);
-sequelize.sync();
+
 module.exports = sequelize;
