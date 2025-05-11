758e63c46db64a27bc784ec177d5d4f3e20f6746
Mathew <mateoicecua2000@gmail.com>
2025-05-10 23:30:24 -0500

    feat(category): add category model, route, schema, and service 🏷️🔌

---
+++ routes/category.router.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const CategoryService = require('../services/category.service');
+const validatorHandler = require('../middlewares/validator.handler');
+
+const {
+  createCategorySchema,
+  updateCategorySchema,
+  getCategorySchema,
+  queryCategorySchema,
+} = require('../schemas/category.schema');
+const router = express.Router();
+const service = new CategoryService();
+
+router.get(
+  '/',
+
+  async (req, res, next) => {
+    try {
+      const categories = await service.find();
+      res.json(categories);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+router.get(
+  '/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      res.json(category);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+router.post(
+  '/',
+  validatorHandler(createCategorySchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.create(body);
+      res.status(201).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+module.exports = router;
