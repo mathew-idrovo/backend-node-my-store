1f87b060f058c27bc89842123d42c4a65d6d61fc
Mathew <mateoicecua2000@gmail.com>
2025-05-14 18:35:01 -0500

    chore: sequilize

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,27 +1,57 @@
-require('dotenv').config(); // ¡primera línea!
-const { config } = require('./../config/config');
+// require('dotenv').config(); // ¡primera línea!
+// const { config } = require('./../config/config');
+// const { Sequelize } = require('sequelize');
+// const setupModels = require('../db/models');
+
+// const USER = encodeURIComponent(process.env.DB_USER);
+// const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+// const HOST = process.env.DB_HOST;
+// const PORT = process.env.DB_PORT;
+// const DB = process.env.DB_NAME;
+
+// const DATABASE_URL = `postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DB}`;
+// const options = {
+//   dialect: 'postgres',
+//   logging: config.isProd ? false : true,
+// };
+// if (config.isProd) {
+//   options.dialectOptions = {
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   };
+// }
+// const sequelize = new Sequelize(DATABASE_URL, options);
+// setupModels(sequelize);
+
+// module.exports = sequelize;
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 const setupModels = require('../db/models');
 
-const USER = encodeURIComponent(process.env.DB_USER);
-const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
-const HOST = process.env.DB_HOST;
-const PORT = process.env.DB_PORT;
-const DB = process.env.DB_NAME;
+const isProd = process.env.NODE_ENV === 'production';
 
-const DATABASE_URL = `postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DB}`;
+const DATABASE_URL = isProd
+  ? process.env.DATABASE_URL
+  : `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 const options = {
   dialect: 'postgres',
-  logging: config.isProd ? false : true,
+  logging: isProd ? false : console.log,
 };
-if (config.isProd) {
+
+if (isProd) {
   options.dialectOptions = {
     ssl: {
+      require: true,
       rejectUnauthorized: false,
     },
   };
 }
+
 const sequelize = new Sequelize(DATABASE_URL, options);
+
 setupModels(sequelize);
 
 module.exports = sequelize;
